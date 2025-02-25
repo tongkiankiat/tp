8d3a6a7620c13c41b2f1bff03a8c4498d36ea155
Liu Xuyan <1739606183@qq.com>
2025-02-26 06:19:42 +0800

    Add abstract superclass Question and subclass FillInTheBlanks. Replace Duke with MindExpander as the main class. Implement "add", "help", "list", and "exit" commands.

---
+++ src/main/java/MindExpander/MultipleChoice.java
@@ -0,0 +1,20 @@
+//package MindExpander;
+//
+//class MultipleChoice extends Question {
+//    private List<String> options;
+//
+//    public MultipleChoice(String question, String answer, List<String> options) {
+//        super(question, answer);
+//        this.options = options;
+//    }
+//
+//    @Override
+//    public boolean checkAnswer(String userAnswer) {
+//        return answer.equalsIgnoreCase(userAnswer.trim());
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "MCQ: " + question + " Options: " + options + " [Answer: " + answer + "]";
+//    }
+//}
