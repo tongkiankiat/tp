87c32deaf00ea56fa6bd39ba80a65112f7642392
Liu Xuyan <1739606183@qq.com>
2025-02-27 20:47:47 +0800

    Add MultipleChoice subclass.

--- src/main/java/MindExpander/MultipleChoice.java
+++ src/main/java/MindExpander/MultipleChoice.java
@@ -1,20 +1,22 @@
-//package MindExpander;
-//
-//class MultipleChoice extends Question {
-//    private List<String> options;
-//
-//    public MultipleChoice(String question, String answer, List<String> options) {
-//        super(question, answer);
-//        this.options = options;
-//    }
-//
-//    @Override
-//    public boolean checkAnswer(String userAnswer) {
-//        return answer.equalsIgnoreCase(userAnswer.trim());
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "MCQ: " + question + " Options: " + options + " [Answer: " + answer + "]";
-//    }
-//}
+package MindExpander;
+
+import java.util.List;
+
+class MultipleChoice extends Question {
+    private List<String> options;
+
+    public MultipleChoice(String question, String answer, List<String> options) {
+        super(question, answer);
+        this.options = options;
+    }
+
+    @Override
+    public boolean checkAnswer(String userAnswer) {
+        return answer.equalsIgnoreCase(userAnswer.trim());
+    }
+
+    @Override
+    public String toString() {
+        return "MCQ: " + question + " Options: " + options + " [Answer: " + answer + "]";
+    }
+}
