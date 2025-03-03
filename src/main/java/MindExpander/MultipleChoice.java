d89ddf34ee2c0bbe13d216ed2edc2358d042467a
Liu Xuyan <1739606183@qq.com>
2025-03-03 16:44:56 +0800

    Add process to handle the addition of a mcq question. MCQ are now displayed with their options shuffled.

--- src/main/java/MindExpander/MultipleChoice.java
+++ src/main/java/MindExpander/MultipleChoice.java
@@ -1,13 +1,15 @@
 package MindExpander;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 class MultipleChoice extends Question {
-    private List<String> options;
+    private final List<String> options;
 
     public MultipleChoice(String question, String answer, List<String> options) {
         super(question, answer);
-        this.options = options;
+        this.options = new ArrayList<>(options);
     }
 
     @Override
@@ -17,6 +19,13 @@ class MultipleChoice extends Question {
 
     @Override
     public String toString() {
-        return "MCQ: " + question + " Options: " + options + " [Answer: " + answer + "]";
+        List<String> shuffledOptions = new ArrayList<>(options);
+        Collections.shuffle(shuffledOptions); // Randomize options each time
+        StringBuilder sb = new StringBuilder();
+        sb.append("MCQ: ").append(question).append("\n");
+        for (int i = 0; i < shuffledOptions.size(); i++) {
+            sb.append((char) ('A' + i)).append(". ").append(shuffledOptions.get(i)).append("\n");
+        }
+        return sb.toString();
     }
 }
