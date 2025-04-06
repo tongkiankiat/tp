c47e129b8c83fc8cf0e9daeb4dd2817f41043f7d
tongkiankiat <tong.kiankiat@gmail.com>
2025-04-06 16:49:44 +0800

    Updates error messages for List and Find, and added messages for Show

--- src/main/java/mindexpander/common/Messages.java
+++ src/main/java/mindexpander/common/Messages.java
@@ -23,24 +23,63 @@ public class Messages {
     public static final String DISPLAYED_INDEX_OFFSET = ". ";
     public static final String EMPTY_INPUT = "Please enter a valid input!";
     public static final String STORAGE_DELIMITER = "%%MINDEXPANDER_DELIM%%";
+    public static final String ERROR_LOGGER_DELIMITER = "|";
 
     // Error Messages
     public static final String LIST_ERROR_MESSAGE =
-            "Invalid command! Please enter either `list` or `list answer` to view the question bank.";
+            "Invalid command! Please enter either " +
+                    "`list`, `list answer`," +
+                    " `list [mcq/fitb/tf]` or `list [mcq/fitb/tf] answer` to view the question bank.";
     public static final String FIND_ERROR_MESSAGE_EMPTY_BODY =
-            "Invalid command! Please enter either `find [KEYWORD]`, `find mcq [KEYWORD]` or `find fitb [KEYWORD]`";
+            "Invalid command! Please enter either "
+                    + "`find [KEYWORD]`, `find mcq [KEYWORD]`, `find fitb [KEYWORD]` or `find tf [KEYWORD]`.";
     public static final String FIND_ERROR_MESSAGE_EMPTY_BODY_MCQ =
             "Invalid command! The correct format should be `find mcq [KEYWORD]`";
     public static final String FIND_ERROR_MESSAGE_EMPTY_BODY_FITB =
             "Invalid command! The correct format should be `find fitb [KEYWORD]`";
+    public static final String FIND_ERROR_MESSAGE_EMPTY_BODY_TF =
+            "Invalid command! The correct format should be `find tf [KEYWORD]`";
+    public static final String UNABLE_TO_INIITIALISE_ERROR_LOGGER = "Unable to initialise the error logger.";
+    public static final String SHOW_ERROR_MESSAGE_EMPTY_BODY =
+            "Invalid command! The correct format should be `show [QUESTION_INDEX]`.";
+    public static final String SHOW_ERROR_NOT_INTEGER =
+            "Invalid command! Please enter an integer for the question index.";
+    public static final String SHOW_ERROR_EMPTY_QUESTION_BANK =
+            "Invalid command! There are currently no questions in the question bank.";
 
     // Strings with parameters
+    public static final String listCommandMessage(String questionType, boolean questionBankIsEmpty) {
+        if (!questionBankIsEmpty) {
+            if (questionType.equals("mcq")) {
+                return "Here are the MCQ questions you have currently:";
+            } else if (questionType.equals("fitb")) {
+                return "Here are the FITB questions you have currently:";
+            } else if (questionType.equals("tf")) {
+                return "Here are the TF questions you have currently:";
+            } else {
+                return "Here are the questions you have currently:";
+            }
+        } else {
+            if (questionType.equals("mcq")) {
+                return "You have no MCQ questions!";
+            } else if (questionType.equals("fitb")) {
+                return "You have no FITB questions!";
+            } else if (questionType.equals("tf")) {
+                return "You have no TF questions!";
+            } else {
+                return "You have no questions!";
+            }
+        }
+    }
+
     public static final String findCommandMessage(String keyword,  String questionType, boolean questionsFound) {
         if (questionsFound) {
             if (questionType.equals("mcq")) {
                 return "Here are the MCQ questions with " + keyword + ":";
             } else if (questionType.equals("fitb")) {
                 return "Here are the FITB questions with " + keyword + ":";
+            } else if (questionType.equals("tf")) {
+                return "Here are the TF questions with " + keyword + ":";
             } else {
                 return "Here are the questions with " + keyword + ":";
             }
@@ -49,9 +88,20 @@ public class Messages {
                 return "No MCQ questions with " + keyword + " found!";
             } else if (questionType.equals("fitb")) {
                 return "No FITB questions with " + keyword + " found!";
+            } else if (questionType.equals("tf")) {
+                return "No TF questions with " + keyword + " found!";
             } else {
                 return "No questions with " + keyword + " found!";
             }
         }
+    }
+
+    public static final String showCommandOutOfRangeMessage(int questionBankCount) {
+        return "Index is out of range! You currently have " + questionBankCount
+                + " questions, please enter a question index in this range.";
+    }
+
+    public static final String showCommandMessage(int questionIndex) {
+        return "Here is the answer for question " + questionIndex + ":";
     }
 }
