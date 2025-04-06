328acc376510923888418b72cca0e81ca5a8bcba
Jensenkuok <jensenkuok2002@gmail.com>
2025-04-06 22:14:07 +0800

    Fixed wildcard import

--- src/test/java/mindexpander/tests/ClearCommandTest.java
+++ src/test/java/mindexpander/tests/ClearCommandTest.java
@@ -9,7 +9,8 @@ import mindexpander.exceptions.IllegalCommandException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ClearCommandTest {
     private QuestionBank questionBank;
