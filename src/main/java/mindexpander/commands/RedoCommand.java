e149b2be2e313b6353d36804f5188852eb7b354e
Liu Xuyan <1739606183@qq.com>
2025-04-06 18:42:25 +0800

    style fix.

--- src/main/java/mindexpander/commands/RedoCommand.java
+++ src/main/java/mindexpander/commands/RedoCommand.java
@@ -2,7 +2,7 @@ package mindexpander.commands;
 
 import mindexpander.data.CommandHistory;
 
-public class RedoCommand extends Command{
+public class RedoCommand extends Command {
 
     public RedoCommand(CommandHistory commandHistory) {
         updateCommandMessage(commandHistory.redo());
